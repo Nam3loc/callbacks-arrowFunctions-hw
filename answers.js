accffc77506262de2b7cc06cb5c37515243280ea
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-11-09 18:10:21 -0500

    finished callbacks and arrow function hw

---
+++ answers.js
@@ -0,0 +1,16 @@
+/*
+
+// ========== Week 7 Day 1- Callbacks and Arrow Functions ========== \\
+
+1) When the add and subtract functions are passed as arguments to the compute function - they are ___ functions.
+    Callback functions
+
+2) In your own words describe what the term "implicit return" means.
+    It means not adding return to a single line of code after an arrow function. You just put what is supposed to be returned and it implicitly returns that statement (ex. const getJob = (projects) => console.log('work hard on ' + projects);)
+
+3) Describe what a callback function is in your own words.
+    A callback function is a function that runs within a function in order to accomplish a specific task like
+        -higher-order functions, doing something with the array you loop through in a forEach statement: clg or function with the looped array
+        -Execute a functino when an event happens (ex. when you click the button, the button turns a different color that you specify within a callback func)
+        -To execute a function that is executed in async functions. It calls the waiting function.... i think lol
+*/
